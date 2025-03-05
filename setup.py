f34cbd0f0a8786326bdf9f935fb72146ef8c9da3
aidan <aidankhogg@gmail.com>
2025-03-05 01:11:14 +0000

    rename of project

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyextendable',
-    version='0.0.1',
+    name='runecaller',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'omegaconf',
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'pyextendable-util=pyextendable.utils.ecosystem:determine_util',
+            'runecaller-util=runecaller.utils.ecosystem:determine_util',
         ],
     },
     package_data={
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     description='A Python project with utility functions and configuration management',
     author='DirtyWork Solutions Limited',
-    author_email='pyextendable@open.dirtywork.solutions',
+    author_email='runecaller@open.dirtywork.solutions',
     url='https://github.com/DirtyWork-Solutions/pyextend',
     classifiers=[
         'Programming Language :: Python :: 3',
